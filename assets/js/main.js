594e18acd7fc1947837309cdb8df169730dd1c6b
oyinkansola <yahayaoyinkansola@gmail.com>
2024-04-07 16:10:15 +0100

    Display success message after successful registration

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
-const newsletterForm = document.querySelector(".newsletter__form");
+const newsletterForm = document.querySelector("#newsletter");
+const subscriptionMessage = document.querySelector("#subscription-message");
 const formInput = document.querySelector(".newsletter__input");
 const formError = document.querySelector(".form-error");
+const dismissBtn = document.querySelector("#dismiss");
+const emailText = document.querySelector(".subscription__email");
 const emailRegex = /^[a-zA-Z]+@[a-zA-Z]+\.[a-zA-Z]{2,4}$/;
 
 newsletterForm.addEventListener("submit", (e) => {
@@ -19,7 +22,11 @@ newsletterForm.addEventListener("submit", (e) => {
   }
 
   if (!formInput.classList.contains("newsletter__input--error")) {
+    emailText.innerHTML = formInput.value;
+
     e.target.reset();
+    newsletterForm.classList.add("d-none");
+    subscriptionMessage.classList.remove("d-none");
   }
 });
 
@@ -28,4 +35,9 @@ formInput.addEventListener("input", (e) => {
     formInput.classList.remove("newsletter__input--error");
     formError.innerHTML = "";
   }
+});
+
+dismissBtn.addEventListener("click", () => {
+  newsletterForm.classList.remove("d-none");
+  subscriptionMessage.classList.add("d-none");
 });
